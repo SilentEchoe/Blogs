57ca5a1ec45fbc776037ab35bb5434f52fe8a2aa
wangkai <33675262+nullniming@users.noreply.github.com>
2018-12-20 14:18:31 +0800

    新建DB 类

---
+++ ToolDB/ToolDBManage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolDB
+{
+    public class ToolDBManage
+    {
+        public bool CreaDB(string path)
+        {
+            try
+            {
+                path = path + ConfigurationManager.AppSettings["tooldb"];
+                SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+                cn.Open();
+                cn.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+
+            }
+
+        }
+    }
+}
