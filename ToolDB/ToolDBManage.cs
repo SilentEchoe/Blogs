6c93390dc05ea99cb6ac6e5b181188faac932afe
wangkai <33675262+nullniming@users.noreply.github.com>
2018-12-20 16:49:45 +0800

    添加配置文件

--- ToolDB/ToolDBManage.cs
+++ ToolDB/ToolDBManage.cs
@@ -17,6 +17,16 @@ namespace ToolDB
                 path = path + ConfigurationManager.AppSettings["tooldb"];
                 SQLiteConnection cn = new SQLiteConnection("data source=" + path);
                 cn.Open();
+                if (cn.State != System.Data.ConnectionState.Open)
+                {
+                    cn.Open();
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.Connection = cn;
+                    // 创建表结构
+                    cmd.CommandText = "CREATE TABLE t1(id varchar(4),score int)";
+                    //cmd.CommandText = "CREATE TABLE IF NOT EXISTS t1(id varchar(4),score int)";
+                    cmd.ExecuteNonQuery();
+                }
                 cn.Close();
                 return true;
             }
