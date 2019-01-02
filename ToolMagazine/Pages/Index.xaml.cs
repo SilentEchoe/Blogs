f7f7ae70d0f4fdc1939a2188a0bfa79f8ea39d8c
wangkai <33675262+nullniming@users.noreply.github.com>
2019-01-02 16:45:31 +0800

    添加富文本流

--- ToolMagazine/Pages/Index.xaml.cs
+++ ToolMagazine/Pages/Index.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,24 @@ namespace ToolMagazine.Pages
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+            string filename = @"C:\Users\Lenovo\Desktop\学习笔记.txt";
+            if (string.IsNullOrEmpty(filename))
+            {
+                throw new ArgumentNullException();
+            }
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException();
+            }
+            using (FileStream stream = File.OpenRead(filename))
+            {
+                TextRange documentTextRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                string dataFormat = DataFormats.Text;
+                StreamReader sr = new StreamReader(stream, Encoding.Default);
+                documentTextRange.Load(new MemoryStream(Encoding.UTF8.GetBytes(sr.ReadToEnd())), dataFormat);
+
+            }
+          
         }
     }
 }
