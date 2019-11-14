e3ed4077e0c0aa36124ace5e6b9250c1078ce192
kai_wang <871779972@qq.com>
2019-11-14 10:27:56 +0800

    make()函数

--- src/mycalc/section.go
+++ src/mycalc/section.go
@@ -9,6 +9,10 @@ import "fmt"
 
 var highRiseBuilding [30]int
 
+// 切片类型可直接声明 语法如下：
+// 声明字符串切片
+//var strList []string
+
 func main() {
 	var a = [3]int{1, 2, 3}
 	// a[0:1] 代表从第一个元素开始 到第二个元素结束
@@ -24,5 +28,13 @@ func main() {
 	fmt.Println(highRiseBuilding[20:])
 	// 开头到中间指定位置的所有元素
 	fmt.Println(highRiseBuilding[:2])
+
+	// 使用Make()函数构造切片
+	// 语法为 make( []Type, size, cap ) size指的是这个类型分配多少个元素，Cap 为预分配的元素数量
+	// 这个值不影响size，只能提前分配空间，降低多次分配空间造成的性能问题
+	c := make([]int, 2)
+	b := make([]int, 2, 10)
+	fmt.Println(c, b)
+	fmt.Println(len(c), len(b))
 
 }
