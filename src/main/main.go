75fb5752f4892b347d87726dd8cf256aebdbbeb6
kai_wang <871779972@qq.com>
2020-01-05 17:39:39 +0800

    函数

--- src/main/main.go
+++ src/main/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	"../model"
 )
 
 func main() {
-	p := model.NewPerson("smith")
-	p.SetAge(18)
-	p.SetSal(5000)
-	fmt.Println(p)
-	fmt.Println(p.Name, " age =", p.GetAge(), " sal = ", p.GetSal())
-
+	// p := model.NewPerson("smith")
+	// p.SetAge(18)
+	// p.SetSal(5000)
+	// fmt.Println(p)
+	// fmt.Println(p.Name, " age =", p.GetAge(), " sal = ", p.GetSal())
+	fmt.Println("hello go")
 }
