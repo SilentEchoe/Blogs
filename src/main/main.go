93ecaeab270d064fce4832d0e67d688cc22abcd5
kai_wang <871779972@qq.com>
2020-01-26 21:23:28 +0800

    指针的赋值

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+// 变量、指针和地址三者的关系是，每个变量都拥有地址，指针的值就是地址。
 func main() {
-	fmt.Println("hello go")	
+	var cat int =1 
+	var str string = "banana"
+	fmt.Printf("%p %p",&cat,&str)
+
+
 }
