6ac740cc69723e6362d2aa022736b648cb81757a
kai_wang <871779972@qq.com>
2020-01-26 20:29:50 +0800

    指针

--- src/main/main.go
+++ src/main/main.go
@@ -5,14 +5,5 @@ import (
 )
 
 func main() {
-	// p := model.NewPerson("smith")
-	// p.SetAge(18)
-	// p.SetSal(5000)
-	// fmt.Println(p)
-	// fmt.Println(p.Name, " age =", p.GetAge(), " sal = ", p.GetSal())
-	fmt.Println("hello go")
-
-
-		
-	
+	fmt.Println("hello go")	
 }
