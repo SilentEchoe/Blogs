498cb79623d707e1c7f14e8144dd7be2b21e2171
kai_wang <871779972@qq.com>
2020-02-05 18:29:32 +0800

    根据类型断言拓展分支

--- src/main/log.go
+++ src/main/log.go
@@ -6,11 +6,23 @@ package main
 import "fmt"
 
 func  main()  {
-	// 申明一个接口
-	var x interface{}
-	x = 10
-	value, ok := x.(int)
+	var a int
+	a = 10
+	// 验证类型
+	getType(a)
+	
+}
 
-	fmt.Print(value, ",", ok)
 
+func getType(a interface{}){
+	switch a.(type){
+		case int:
+			fmt.Println("the type of a is int")
+		case string:
+			fmt.Println("the type of a is string")
+		case float64:
+			fmt.Println("the type of a is float64")
+		default:
+			fmt.Println("unknown type")
+	}
 }
