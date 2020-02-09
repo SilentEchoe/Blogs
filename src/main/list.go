78450e2bf97296fc5a0efa70665bd76f53a6aa3a
kai_wang <871779972@qq.com>
2020-02-09 21:32:50 +0800

    list 排序

--- src/main/list.go
+++ src/main/list.go
@@ -1,7 +1,48 @@
 package main
 
-type Interface interface {
-	Len() int //获取元素数量
-	Less(i,j int)bool // i,j是序列元素的指数
-	Swap(i,j int) // 交换元素
+import (
+	"fmt"
+	"sort"
+)
+
+// 将[]string定义为MyStringList类型
+
+type MyStringList []string
+
+// 获取元素数量
+func (m MyStringList) Len() int{
+	return len(m)
+}
+
+// 比较元素大小
+func (m MyStringList) Less(i,j int) bool  {
+	return m[i]<m[j]
+}
+
+// 实现元素交换
+
+func (m MyStringList) Swap(i,j int)  {
+	m[i],m[j] = m[j],m[i]
+}
+
+func main()  {
+	//准备一个切片
+
+	names := MyStringList{
+		"3. Triple Kill",
+		"5. Penta Kill",
+		"2. Double Kill",
+		"4. Quadra Kill",
+		"1. First Blood",
+	}
+
+	sort.Sort(names)
+
+	for _, v := range names {
+		fmt.Printf("%s\n",v)
+	}
+
+
+
+
 }
