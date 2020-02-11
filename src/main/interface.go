a245994fabccc02d94cfd9d1095ead6aa6f20a15
kai_wang <871779972@qq.com>
2020-02-11 19:44:51 +0800

    接口实现

--- src/main/interface.go
+++ src/main/interface.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // 定义一个接口
 //type class interface{
@@ -15,22 +17,34 @@ type DataWriter interface {
 type file struct {
 }
 
+type Log struct {
+}
+
 // 实现DataWriter接口的 WriteData
 func (d *file)  WriteData(data interface{}) error {
 	fmt.Println("WriteData",data)
 	return nil
 }
 
-func main()  {
-	f := new(file)
+func (d *Log)  WriteData(data interface{}) error {
+	fmt.Println("WriteData",data)
+	return nil
+}
 
+
+func main()  {
+	// 实例一个构造
+	f := new(file)
 	//声明一个接口
 	var writer DataWriter
-
 	writer = f
-
 	writer.WriteData("data")
 
+	l := new(Log)
+	//声明一个接口
+	var writer2 DataWriter
+	writer2 = l
+	writer2.WriteData("data")
 
 }
 
