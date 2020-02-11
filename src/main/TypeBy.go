0db86091015bb6702eeaeb2b0c3d9359313f951b
kai_wang <871779972@qq.com>
2020-02-11 18:53:18 +0800

    实现接口

--- src/main/TypeBy.go
+++ src/main/TypeBy.go
@@ -1,10 +1,27 @@
 package main
 
-import "io"
+//i 代表接口变量，T 代表转换的目标类型，t 代表转换后的变量。
+//t := i.(T)
 
-var w io.Writer
+import "fmt"
 
-w = os.Stdout
+// 定义飞行动物接口
+type  Flyer interface {
+	Fly()
+}
 
-f := w.(*os.File)
-c := w.(*bytes.Buffer)
+// 定义行走动物接口
+type Walker interface {
+	Walk()
+}
+
+// 定义鸟类
+type bird struct {
+}
+
+// 实现飞行动物接口
+func (b *bird) Fly()  {
+	fmt.Println("bird:fly")
+}
+
+// 为鸟
