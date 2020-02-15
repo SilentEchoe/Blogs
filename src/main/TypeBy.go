04ad6635a0430a68fe98ce4120570cf2acfb63e1
kai_wang <871779972@qq.com>
2020-02-15 18:03:05 +0800

    遍历切片

--- src/main/TypeBy.go
+++ src/main/TypeBy.go
@@ -10,9 +10,13 @@ import "fmt"
 func main()  {
 	source := []string{"A1","A2","A3","A4","A5"}
 
+	// 截取数组的第三个元素
 	slice := source[2:3:3]
 	fmt.Println(slice)
 	// 追加字段
 	slice = append(slice,"kai")
-	fmt.Println(slice)
+	for index,value := range slice {
+		fmt.Printf("Index: %d value: %d\n",index,value)
+
+	}
 }
