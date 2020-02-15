1feb5a1e2e4c349047352734ac925475e6efc083
kai_wang <871779972@qq.com>
2020-02-15 17:50:06 +0800

    append

--- src/main/TypeBy.go
+++ src/main/TypeBy.go
@@ -1,76 +1,18 @@
-package main
+// 类型断言的格式
+// i代表接口变量,T代表转换的目标类型,t 代表转换后的变量。
+// t := i.(T)
 
-//i 代表接口变量，T 代表转换的目标类型，t 代表转换后的变量。
-//t := i.(T)
+package main
 
 import "fmt"
 
-// 定义飞行动物接口
-type  Flyer interface {
-	Fly()
-}
 
-// 定义行走动物接口
-type Walker interface {
-	Walk()
-}
-
-// 定义鸟类
-type bird struct {
-}
-
-// 实现飞行动物接口
-func (b *bird) Fly()  {
-	fmt.Println("bird:fly")
-}
-
-// 为鸟添加Walk()方法,实现行走动物接口
-func (b *bird) Walk()  {
-	fmt.Println("bird:walk")
-}
-
-type pig struct {
-}
-
-// 为猪添加Walk()方法，实现行走动物接口
-func (p *pig) Walk()  {
-	fmt.Println("pig:walk")
-}
-
-//将接口转换为其他接口
 func main()  {
-	// 创建动物的名字到实例的映射
-	animals := map[string]interface{}{
-		"bird":new(bird),
-		"pig" : new(pig),
-	}
+	source := []string{"A1","A2","A3","A4","A5"}
 
-
-	for name,obj := range animals{
-		f, isFlyer := obj.(Flyer)
-		w, isWalker := obj.(Walker)
-
-		fmt.Printf("name: %s isFlyer: %v isWalker: %v\n", name, isFlyer, isWalker)
-
-		if isFlyer  {
-			f.Fly()
-		}
-
-		if isWalker {
-			w.Walk()
-		}
-
-	}
-
-
-
+	slice := source[2:3:3]
+	fmt.Println(slice)
+	// 追加字段
+	slice = append(slice,"kai")
+	fmt.Println(slice)
 }
-
-
-
-
-
-
-
-
-
