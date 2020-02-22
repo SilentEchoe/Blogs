c595e92902df161f4891c45b8a9f522799c23f35
kai_wang <871779972@qq.com>
2020-02-22 16:47:52 +0800

    gin框架测试

--- src/main/main.go
+++ src/main/main.go
@@ -6,12 +6,13 @@ import (
 )
 
 
-func main() {
+func main(){
 	router := gin.Default()
-
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World!")
-	})
-
+	router.GET("/user/:name/:password", ginHandler) //冒号加上一个参数名组成路由参数
 	router.Run(":8000")
+}
+func ginHandler(c *gin.Context) {
+	name := c.Param("name")   //读取name的值
+	pwd := c.Param("password")
+	c.String(http.StatusOK, " Hello  %s,%s", name, pwd)     //http.StatusOK返回状态码
 }
