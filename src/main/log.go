a51230526739a8c539099afc0a87c1b897485e13
kai_wang <871779972@qq.com>
2020-02-23 23:38:29 +0800

    添加注释

--- src/main/log.go
+++ src/main/log.go
@@ -3,8 +3,12 @@ package main
 import "log"
 
 func init()  {
-	// 设置
+	// 设置日志项的前缀
 	log.SetPrefix("TRACE：")
+	// 标识用于控制日志项信息
+	// Ldate 代表日期
+	// Lmicroseconds 代表毫秒级别相应时间
+	// Llongfile 代表完整的文件名和行号
 	log.SetFlags(log.Ldate | log.Lmicroseconds|log.Llongfile)
 }
 
