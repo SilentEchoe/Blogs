bab833bbb3db19f3054d7e57fe4105350c1fe81e
kai_wang <871779972@qq.com>
2020-02-23 23:32:44 +0800

    Log包的使用

--- src/main/log.go
+++ src/main/log.go
@@ -1,1 +1,15 @@
 package main
+
+import "log"
+
+func init()  {
+	// 设置
+	log.SetPrefix("TRACE：")
+	log.SetFlags(log.Ldate | log.Lmicroseconds|log.Llongfile)
+}
+
+func main()  {
+	log.Println("message")
+	log.Fatalln("fatal message")
+	log.Panicln("Panic message")
+}
