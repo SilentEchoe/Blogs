ce90c8448ae0632ece0baaa9421485879b4a4965
kai_wang <871779972@qq.com>
2020-02-25 01:15:10 +0800

    添加tag 基础方法

--- routers/api/tag.go
+++ routers/api/tag.go
@@ -1,5 +1,25 @@
-package main
+package v1
 
-func main() {
-	
+import "github.com/gin-gonic/gin"
+
+// 获取多个文章标签
+func GetTags(c *gin.Context)  {
+
 }
+
+// 新增文章标签
+func AddTag(c *gin.Context)  {
+
+}
+
+// 更改文章标签
+func EditTag(c *gin.Context)  {
+
+}
+
+// 删除文章标签
+func DeleteTag(c *gin.Context)  {
+
+}
+
+
