fdf96b548dd3e516e57da93603114c82279938af
kai_wang <871779972@qq.com>
2020-02-26 15:57:35 +0800

    测试请求

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
-
+	"fmt"
+	"net/http"
 	"github.com/gin-gonic/gin"
+	"LearningNotes-Go/pkg/setting"
 )
 
 func main() {
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "test",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
