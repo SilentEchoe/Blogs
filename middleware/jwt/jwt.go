41bccd8805be523cd5d8c363e8faa3d88db48b46
kai_wang <871779972@qq.com>
2020-03-03 01:54:39 +0800

    填写验证token的中间件

---
+++ middleware/jwt/jwt.go
@@ -0,0 +1,47 @@
+package jwt
+
+import (
+	"LearningNotes-Go/pkg/e"
+	"LearningNotes-Go/pkg/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		var data interface{}
+
+		code = e.SUCCESS
+		token := c.Query("token")
+
+		// 如果token是空的
+		if token == "" {
+			code = e.INVALID_PARAMS
+		} else {
+			// 如果不为空就验证token
+			claims, err := util.ParseToken(token)
+			if err != nil {
+				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			}
+
+		}
+
+		// 如果状态不为成功 就返回异常信息
+		if code != e.SUCCESS {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  e.GetMsg(code),
+				"data": data,
+			})
+
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
