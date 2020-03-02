673cea5fc215f32e40db73919b503dd4019b6cbb
kai_wang <871779972@qq.com>
2020-03-02 17:12:50 +0800

    添加 修改文章标签函数

--- models/tag.go
+++ models/tag.go
@@ -48,6 +48,8 @@ func AddTag(name string, state int, createBy string) bool {
 	return true
 }
 
+// gorm所支持的回调方法
+// 创建时间戳
 func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
 	scope.SetColumn("CreatedOn", time.Now().Unix())
 	return nil
@@ -56,4 +58,24 @@ func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
 func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
 	scope.SetColumn("ModifiedOn", time.Now().Unix())
 	return nil
+}
+
+func ExistTagByID(id int) bool {
+	var tag Tag
+	// 查询标签
+	db.Select("id").Where("id = ?", id).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func DeleteTag(id int) bool {
+	db.Where("id=?", id).Delete(&Tag{})
+	return true
+}
+
+func EditTag(id int, data interface{}) bool {
+	db.Model(&Tag{}).Where("id = ?", id).Updates(data)
+	return true
 }
