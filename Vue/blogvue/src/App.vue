5b9f426a1d8827972a369a4a56fe167a6e38afaf
kai_wang <871779972@qq.com>
2020-03-05 06:44:06 +0800

    重新生成项目

--- Vue/blogvue/src/App.vue
+++ Vue/blogvue/src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-     <users />
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<style lang="stylus">
- @import "./style/stylehome.css";//就是在这里引用
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- Vue/blogvue/src/main.js
+++ Vue/blogvue/src/main.js
@@ -1,16 +1,8 @@
-import Users from "./components/Users"
-
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.component("users",Users)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
