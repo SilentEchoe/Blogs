96c829c6dbfa8915083e9aa56239f2e15ecb9621
kai_wang <871779972@qq.com>
2020-03-05 17:01:56 +0800

    Vue:添加iview

--- Vue/blogvue/src/components/Users.vue
+++ Vue/blogvue/src/components/Users.vue
@@ -1,16 +1,46 @@
 <template>
     <div>
-        User!!!
+        <div id="Top">
+            <Button>Default</Button>
+            顶部
+        </div>
+        
+        <div id="CenTen">中部内容</div>
+        
+        <div id="Bottom">底部内容</div>
+       
     </div>
 </template>
 
 
+
 <script>
+import { Button, Table } from 'view-design';
+
 export default {
     
 }
 </script>
 
-<style >
+<style  scoped>
+#Top{
+    width: 100%;
+    height: 20%;
+    
+}
+
+#CenTen{
+    width: 100%;
+    height: 70%;
+    
+}
+
+#Bottom{
+    width: 100%;
+    height: 10%;
+    
+}
+
+</style>>
 
 </style>
