1e5fe6fdf1b2a457b5499ed109dc80503b0e7cf3
kai_wang <871779972@qq.com>
2020-03-09 06:18:37 +0800

    添加log

--- pkg/logging/file.go
+++ pkg/logging/file.go
@@ -1,1 +1,54 @@
 package logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	LogSavePath = "runtime/logs/"
+	LogSabeName = "log"
+	LogFileExt  = "log"
+	TimeFormat  = "20200309"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
+
+func getLogFileFullPath() string {
+	prefixPath := getLogFilePath()
+	suffixPath := fmt.Sprintf("%s%s.%s", LogSabeName, time.Now().Format(TimeFormat), LogFileExt)
+
+	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+}
+
+// 打开log文件
+func openLogFile(filePath string) *os.File {
+	_, err := os.Stat(filePath)
+
+	switch {
+	// 判断文件是否存在
+	case os.IsNotExist(err):
+		mkDir()
+	case os.IsPermission(err):
+		log.Fatalf("Permission: %v", err)
+	}
+
+	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Fail to OpenFile:%v", err)
+	}
+	return handle
+
+}
+
+func mkDir() {
+	dir, _ := os.Getwd()
+	err := os.MkdirAll(dir+"/"+getLogFilePath(), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
