190e1f51512bf6b7071fe687382a9473ae78c218
kai_wang <871779972@qq.com>
2020-04-09 09:21:52 +0800

    删除modelattrvalue

--- models/ModelBins.go
+++ models/ModelBins.go
@@ -1,1 +1,30 @@
 package models
+
+type MadalenaAttrValue struct {
+	Model
+
+	MadalenaTypeId string `json:"madalena_type_id"`
+	attrKey        string `json:"attr_key"`
+	attrValue      string `json:"attr_value"`
+	Bins           int    `json:"bin_template"`
+	Version        string `json:"version"`
+	Sn             string `json:"sn"`
+}
+
+type Bins struct {
+	Model
+
+	MadalenaAttrValueId string `json:"madalena_attr_value_id"`
+	FilePath            string `json:"file_location"`
+	FileName            string `json:"file_name"`
+	IsDelete            int    `json:"is_delete"`
+	BinFiles            string `json:"base_64"`
+}
+
+// 查找bin文件
+func GetBin(pageNum int, pageSize int, maps interface{}) (bins []Bins) {
+	db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&bins)
+	return
+}
+
+// 查找
--- routers/api/v1/ModelBins.go
+++ routers/api/v1/ModelBins.go
@@ -1,6 +1,9 @@
 package v1
 
 import (
+	"LearningNotes-Go/models"
+	"LearningNotes-Go/pkg/setting"
+	"LearningNotes-Go/pkg/util"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,5 +15,15 @@ import (
 // @Success 200 {string} string "{"code":200,"data":{},"msg":"ok"}"
 // @Router /api/v1/moduleNames [Get]
 func GetModelBins(c *gin.Context) {
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+	maps["modelId"] = c.PostForm("modelId")
+	maps["compatibilityType"] = c.PostForm("compatibilityType")
+	maps["attrKey"] = c.PostForm("attrKey")
+	maps["attrValue"] = c.PostForm("attrValue")
+
+	data["lists"] = models.GetModelNames(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetModelNameTotal(maps)
 
 }
