2c64e6fbcd15a7f4e752e98f9475f0cd62aec0e8
kai_wang <871779972@qq.com>
2020-09-16 00:04:15 +0800

    todo

--- leetcode/TwoNumbers/addTwoNumbers.go
+++ leetcode/TwoNumbers/addTwoNumbers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
+	"strings"
 )
 
 /**
@@ -30,13 +32,45 @@ func main() {
 	node2.Val = 3
 	node1.Next = node2
 
-	addTwoNumbers(nodeLink, nodeLink)
+	nodeLink2 := new(ListNode)
+	nodeLink2.Val = 5
+
+	node11 := new(ListNode)
+	node11.Val = 6
+	nodeLink2.Next = node11
+
+	node22 := new(ListNode)
+	node22.Val = 4
+	node11.Next = node22
+
+	nowlistNode := addTwoNumbers(nodeLink, nodeLink2)
+
+	for {
+		if nowlistNode != nil {
+			fmt.Print(nowlistNode.Val)
+			nowlistNode = nowlistNode.Next
+		} else {
+			break
+		}
+	}
 
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var strl1 = LinkedToString(l1)
+
+	var strl2 = LinkedToString(l2)
+	int1, _ := strconv.ParseInt(strl1, 10, 64)
+	int2, _ := strconv.ParseInt(strl2, 10, 64)
+	str3 := strconv.FormatInt(int1+int2, 10)
+
+	NewNode(strings.Fields(str3))
+	return nil
+}
+
+func LinkedToString(l1 *ListNode) string {
 	var strl1 string
-	var strl2 string
+
 	nowlistNode := l1
 
 	for {
@@ -48,22 +82,21 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			break
 		}
 	}
+	return strl1
+}
 
-	nowlistNode2 := l2
+// 创建N个节点的循环链表
+func NewNode(str []string) *ListNode {
+	//p := new(ListNode)
+	for i := 0; i < len(str); i++ {
+		int1, _ := strconv.Atoi(str[i])
+		fmt.Println(int1)
+		/*int1, _ := strconv.Atoi(str[i])
+		r := new(ListNode)
+		r.Val = int1
+		p = r
+		p.Next = r*/
 
-	for {
-		if nowlistNode2 != nil {
-			str21 := strconv.Itoa(nowlistNode2.Val)
-			strl2 = str21 + strl2
-			nowlistNode2 = nowlistNode2.Next
-		} else {
-			break
-		}
 	}
-
-	int1, _ := strconv.ParseInt(strl1, 10, 64)
-	int2, _ := strconv.ParseInt(strl2, 10, 64)
-
-	println(int1 + int2)
 	return nil
 }
