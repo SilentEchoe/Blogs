1de697d93a6433bbe98fb6791c18121fa52f775a
kai_wang <871779972@qq.com>
2020-09-15 22:29:24 +0800

    合并两个链表

--- leetcode/TwoNumbers/addTwoNumbers.go
+++ leetcode/TwoNumbers/addTwoNumbers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"strconv"
 )
 
@@ -38,27 +36,34 @@ func main() {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	var strl1 string
+	var strl2 string
 	nowlistNode := l1
 
 	for {
 		if nowlistNode != nil {
-			string := strconv.Itoa(l1.Val)
-			strl1 = string + strl1
-
+			str21 := strconv.Itoa(nowlistNode.Val)
+			strl1 = str21 + strl1
 			nowlistNode = nowlistNode.Next
 		} else {
 			break
 		}
 	}
 
-	int64, err := strconv.ParseInt(strl1, 10, 64)
+	nowlistNode2 := l2
 
-	if err != nil {
-		// handle error
-		fmt.Println(err)
-		os.Exit(2)
+	for {
+		if nowlistNode2 != nil {
+			str21 := strconv.Itoa(nowlistNode2.Val)
+			strl2 = str21 + strl2
+			nowlistNode2 = nowlistNode2.Next
+		} else {
+			break
+		}
 	}
 
-	println(int64)
+	int1, _ := strconv.ParseInt(strl1, 10, 64)
+	int2, _ := strconv.ParseInt(strl2, 10, 64)
+
+	println(int1 + int2)
 	return nil
 }
