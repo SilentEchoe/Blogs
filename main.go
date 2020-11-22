80bc350a9d1060115a8855a1dc6ae2fcf9d221bd
kai_wang <871779972@qq.com>
2020-11-22 10:40:18 +0800

    更改问答

--- main.go
+++ main.go
@@ -3,18 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{6, 7, 8}
-	myMap := make(map[int]*int)
-
-	for index, value := range slice {
-		myMap[index] = &value
-	}
-	fmt.Println("=====new map=====")
-	printMap(myMap)
-}
-
-func printMap(myMap map[int]*int) {
-	for key, value := range myMap {
-		fmt.Printf("map[%v]=%v\n", key, *value)
-	}
+	fmt.Println("hello world")
 }
