5538cb92ab8491ae830afb30970c9fbee52d2cb5
kai_wang <871779972@qq.com>
2020-11-25 10:28:15 +0800

    并发编程 结构体嵌入Mutex

---
+++ Concurrence/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Mutex 嵌入式
+type Counter struct {
+	sync.Mutex
+	Count uint64
+}
+
+func main() {
+	var counter Counter
+	// 使用WaitGroup等待10个goroutine完成
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			// 对变量count执行10次加1
+			for j := 0; j < 100000; j++ {
+				counter.Lock()
+				counter.Count++
+				counter.Unlock()
+			}
+		}()
+	}
+	// 等待10个goroutine完成
+	wg.Wait()
+	fmt.Println(counter.Count)
+}
