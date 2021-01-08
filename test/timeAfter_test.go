7a54c68fbd463a20e79a2fb9380cf9461595e821
kai_wang <871779972@qq.com>
2021-01-08 16:05:55 +0800

    设置缓冲区

--- test/timeAfter_test.go
+++ test/timeAfter_test.go
@@ -25,6 +25,27 @@ func timeout(f func(chan bool)) error {
 
 }
 
+// 创建有缓存区的channel
+func timeoutWithBuffer(f func(chan bool)) error {
+	done := make(chan bool, 1)
+	go f(done)
+	select {
+	case <-done:
+		fmt.Println("done")
+		return nil
+	case <-time.After(time.Millisecond):
+		return fmt.Errorf("timeout")
+	}
+}
+
+func TestBufferTimeout(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		timeoutWithBuffer(doBadthing)
+	}
+	time.Sleep(time.Second * 2)
+	t.Log(runtime.NumGoroutine())
+}
+
 func test(t *testing.T, f func(chan bool)) {
 	t.Helper()
 	for i := 0; i < 1000; i++ {
