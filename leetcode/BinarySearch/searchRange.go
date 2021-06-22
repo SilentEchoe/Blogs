a9bc121dd65539b643b2939868f9c69a99d191bc
kai_wang <871779972@qq.com>
2021-06-22 23:40:50 +0800

    二分法

--- leetcode/BinarySearch/searchRange.go
+++ leetcode/BinarySearch/searchRange.go
@@ -13,10 +13,21 @@ import "fmt"
  * @return: a list of length 2, [index1, index2]
  */
 func searchRange(A []int, target int) []int {
-
 	res := make([]int, 0)
 	start := 0
 	end := len(A) - 1
+
+	if A[start] == target {
+		res = append(res, start)
+	}
+
+	if A[end] == target {
+		res = append(res, end)
+	}
+
+	if len(res) == 2 {
+		return res
+	}
 
 	for start+1 < end {
 		mid := start + (end-start)/2
@@ -28,14 +39,24 @@ func searchRange(A []int, target int) []int {
 		} else if A[mid] > target {
 			end = mid
 		}
+
+	}
+
+	if len(res) == 1 {
+		res = append(res, res[0])
+	}
+
+	if len(res) < 2 {
+		res = append(res, -1)
+		res = append(res, -1)
 	}
 
 	return res
 }
 
 func main() {
-	var target = 8
-	var A = []int{5, 7, 7, 8, 8, 10}
+	var target = 2
+	var A = []int{-1, 0, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 90, 92, 93, 101}
 	res := searchRange(A, target)
 	for _, v := range res {
 		fmt.Println(v)
