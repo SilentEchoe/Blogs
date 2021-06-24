fffa94653c154940ccdaf046bfda5cfa0ad43e2d
kai_wang <871779972@qq.com>
2021-06-24 22:30:23 +0800

    create stun

--- Stun/main.go
+++ Stun/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/ccding/go-stun/stun"
+)
+
+const (
+	ServiceAddress  = "10.32.130.131"
+	ServiceHostName = "Service"
+	ServiceHost     = 3478
+	SoftwareName    = "TestStunClient"
+)
+
+func main() {
+	conn := stun.NewClient()
+	conn.SetServerAddr(ServiceAddress)
+	conn.SetServerHost(ServiceHostName, ServiceHost)
+	conn.SetSoftwareName(SoftwareName)
+	conn.Discover()
+}
