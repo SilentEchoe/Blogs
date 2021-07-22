d8656e73f8fb1d1ca554dadb06c9fc07cc73c862
kai_wang <871779972@qq.com>
2021-07-22 20:16:50 +0800

    slice add slice

--- Interview/aboutSlice.go
+++ Interview/aboutSlice.go
@@ -7,7 +7,7 @@ func main() {
 }
 
 // 这个面试提主要看两点
-// 1. 切片的初始化是否合法
+// 1. for range 循环的时候会创建每个元素的副本,不是每个元素的引用，所以取值时都是变量最后赋的值
 // 2. map中的顺序是随机的，不是有序的，这点需要注意
 func SliceTestOne() {
 	slice := []int{0, 1, 2, 3}
@@ -21,4 +21,12 @@ func SliceTestOne() {
 		fmt.Println(key, "->", *value)
 	}
 
+}
+
+// 切片和切片相加
+func SliceAdding() {
+	s1 := []int{1, 2, 3}
+	s2 := []int{4, 5}
+	s1 = append(s1, s2...)
+	fmt.Println(s1)
 }
