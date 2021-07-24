75331bbb6de245d70b64ef2e0b3e0546ffba6063
kai_wang <871779972@qq.com>
2021-07-24 20:14:23 +0800

    SliceDilatation

---
+++ Interview/aboutInt.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	low_index := int(0x0F)
+	hight_index := int(0xF0)
+
+	fmt.Println(low_index, hight_index)
+}
--- Interview/aboutSlice.go
+++ Interview/aboutSlice.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	SliceDilatation()
 	SliceTestOne()
 }
 
-// 这个面试提主要看两点
+// SliceTestOne 这个面试提主要看两点
 // 1. for range 循环的时候会创建每个元素的副本,不是每个元素的引用，所以取值时都是变量最后赋的值
 // 2. map中的顺序是随机的，不是有序的，这点需要注意
 func SliceTestOne() {
@@ -23,10 +24,29 @@ func SliceTestOne() {
 
 }
 
-// 切片和切片相加
+// SliceAdding 切片和切片相加
 func SliceAdding() {
 	s1 := []int{1, 2, 3}
 	s2 := []int{4, 5}
 	s1 = append(s1, s2...)
 	fmt.Println(s1)
+}
+
+// SliceDilatation 切片扩容
+// 这要主要是的 append 方法是在函数内部,底层数组和外面的不是一个，所以没有改变外面的。
+func SliceDilatation() {
+	a := []int{7, 8, 9}
+	fmt.Printf("%+v\n", a)
+	ap(a)
+	fmt.Printf("%+v\n", a)
+	app(a)
+	fmt.Printf("%+v\n", a)
+}
+
+func ap(a []int) {
+	a = append(a, 10)
+}
+
+func app(a []int) {
+	a[0] = 1
 }
