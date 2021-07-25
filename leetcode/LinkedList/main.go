b4fe64597b230c2c878c9bcab68695b9d9c4023c
kai_wang <871779972@qq.com>
2021-07-25 18:36:15 +0800

    add LinkedList

--- leetcode/LinkedList/main.go
+++ leetcode/LinkedList/main.go
@@ -1,1 +1,56 @@
-package LinkedList
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	nodeThree := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+
+	nodeTwo := &ListNode{
+		Val:  1,
+		Next: nodeThree,
+	}
+
+	nodeHead := &ListNode{
+		Val:  1,
+		Next: nodeTwo,
+	}
+	fmt.Println(*deleteDuplicatesTwo(nodeHead))
+}
+
+/*
+单向链表
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil {
+		// 全部删除完再移动到下一个元素
+		for current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	return head
+}
+
+// 请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+func deleteDuplicatesTwo(head *ListNode) *ListNode {
+	current := head
+	for current != nil {
+		// 全部删除完再移动到下一个元素
+		for current.Next != nil && current.Val == current.Next.Val {
+			current = current.Next.Next
+			return current
+		}
+		current = current.Next
+	}
+	return head
+}
