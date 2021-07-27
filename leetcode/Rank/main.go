44179912270acb36121f319db4591c47f0870d73
wangkai <871779972@qq.com>
2021-07-27 17:42:58 +0800

    add 快速排序

--- leetcode/Rank/main.go
+++ leetcode/Rank/main.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
-	arry := []int{1, 3, 4, 7, 2, 1, 9}
-	fmt.Println(bubbleSort(arry))
+	arry := []int{1, 3, 4,123,56, 7, 2, 1, 9}
+	fmt.Println("冒泡排序：", bubbleSort(arry))
+	fmt.Println("快速排序：",Quicksort(arry))
 }
 
 // 冒泡排序
@@ -23,4 +24,28 @@ func bubbleSort(arry []int) []int {
 		}
 	}
 	return arry
+}
+
+// 快速排序 
+func Quicksort (arry []int)  []int {
+	if len(arry) <= 1 {
+		return arry
+	}
+
+	special := arry[0]
+	mid := make([]int,0)
+	hight := make([]int,0)
+	low := make([]int,0)
+	for i:=0; i< len(arry); i++ {
+		if arry[i] == special {
+			mid = append(mid,arry[i])
+		}else if arry[i] < special {
+			low = append(low,arry[i])
+		}else if arry[i] > special {
+			hight = append(hight,arry[i])
+		}
+	}
+	low,hight = Quicksort(low), Quicksort(hight)
+	myres := append(append(low,mid...),hight...)
+	return myres
 }
