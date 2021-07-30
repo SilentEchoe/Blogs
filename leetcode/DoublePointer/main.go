45850726141fe1786e18e07b1ef01145c34a9b03
wangkai <871779972@qq.com>
2021-07-30 10:39:45 +0800

    add rotate func

--- leetcode/DoublePointer/main.go
+++ leetcode/DoublePointer/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	nums := []int{-4, -1, 0, 3, 10}
 	fmt.Println(sortedSquares(nums))
+	rotateNums := []int{1, 2, 3, 4, 5, 6, 7}
+	rotate(rotateNums, 3)
+	fmt.Println(rotateNums)
+
 }
 
 //输入：nums = [-4,-1,0,3,10]
@@ -34,4 +38,23 @@ func WitBranch(n int) int {
 		return -n
 	}
 	return n
+}
+
+// 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+// 输入: nums = [1,2,3,4,5,6,7], k = 3
+//输出: [5,6,7,1,2,3,4]
+//解释:
+//向右旋转 1 步: [7,1,2,3,4,5,6]
+//向右旋转 2 步: [6,7,1,2,3,4,5]
+//向右旋转 3 步: [5,6,7,1,2,3,4]
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/rotate-array
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+func rotate(nums []int, k int) {
+	newNums := make([]int, len(nums))
+	for i, v := range nums {
+		newNums[(i+k)%len(nums)] = v
+	}
+	copy(nums, newNums)
 }
