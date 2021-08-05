917d154b55ebcfbe0f893ee9de2d0318cc236011
kai_wang <871779972@qq.com>
2021-08-05 23:09:08 +0800

    备份

--- leetcode/LinkedList/main.go
+++ leetcode/LinkedList/main.go
@@ -23,6 +23,8 @@ func main() {
 		Next: nodeTwo,
 	}
 	fmt.Println(*deleteDuplicatesTwo(nodeHead))
+
+	//middleNode(nodeHead)
 }
 
 /*
@@ -51,4 +53,25 @@ func deleteDuplicatesTwo(head *ListNode) *ListNode {
 		}
 	}
 	return head
+}
+
+//输入：[1,2,3,4,5]
+//输出：此列表中的结点 3 (序列化形式：[3,4,5])
+//返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+//注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+//ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+
+func middleNode(head *ListNode) *ListNode {
+	current := head
+	count := 0
+	for current != nil {
+		count++
+		// 全部删除完再移动到下一个元素
+		for current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	fmt.Println(count)
+	return current
 }
