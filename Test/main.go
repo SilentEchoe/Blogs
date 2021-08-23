7f21d503b731017b4877505b2c620963ffff0536
wangkai <871779972@qq.com>
2021-08-23 17:42:13 +0800

    接口测试

--- Test/main.go
+++ Test/main.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 type GameBase interface {
-	OnInit()
-	OnBegin()
+	OnInit()  // 初始化游戏
+	OnBegin() // 游戏数据同步
 }
 
-type LOL struct {
+type Game_LOL struct {
 }
 
-func (l LOL) OnInit() {
+func (l *Game_LOL) OnInit() {
 	fmt.Println("LOL OnInit")
 }
 
-func (l LOL) OnBegin() {
+func (l *Game_LOL) OnBegin() {
 	fmt.Println("LOL OnBegin")
 }
 
 func main() {
 	var gameBase GameBase
-	gameBase = new(LOL)
+	gameBase = new(Game_LOL)
 	gameBase.OnInit()
 }
