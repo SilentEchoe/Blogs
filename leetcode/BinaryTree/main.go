c0fed4a0cfa1aba6dce74451bacaadb82bff9b31
kai_wang <871779972@qq.com>
2021-10-08 16:21:50 +0800

    add new func binaryTree

--- leetcode/BinaryTree/main.go
+++ leetcode/BinaryTree/main.go
@@ -1,6 +1,11 @@
 package main
 
 // 二叉树算法题
+// 	   4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
 
 type TreeNode struct {
 	Val int
@@ -9,7 +14,49 @@ type TreeNode struct {
 }
 
 func main()  {
-	
+	var one = TreeNode{
+		Val: 1,
+		Left : nil,
+		Right : nil,
+	}
+
+	var three = TreeNode{
+		Val: 3,
+		Left : nil,
+		Right : nil,
+	}
+
+	var two = TreeNode{
+		Val: 2,
+		Left : &one,
+		Right : &three,
+	}
+
+	var six = TreeNode{
+		Val: 6,
+		Left : nil,
+		Right : nil,
+	}
+
+	var nine = TreeNode{
+		Val: 9,
+		Left : nil,
+		Right : nil,
+	}
+
+	var seven = TreeNode{
+		Val: 7,
+		Left : &six,
+		Right : &nine,
+	}
+
+	var head = TreeNode{
+		Val: 4,
+		Left : &two,
+		Right : &seven,
+	}
+
+	mirrorTree(&head)
 }
 
 //输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
@@ -36,3 +83,16 @@ func maxDepth(root *TreeNode) int {
 	}
 	return right + 1
 }
+
+// 输入一个二叉树,反转它
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := mirrorTree(root.Left)
+	right := mirrorTree(root.Right)
+	root.Left = right
+	root.Right = left
+	return  root
+}
+
