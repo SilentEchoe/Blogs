322aa0ea54848351ef24de170cc1cb62afddc438
kai_wang <871779972@qq.com>
2021-11-27 20:18:03 +0800

    递归翻转链表

--- leetcode/LinkedList/main.go
+++ leetcode/LinkedList/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -229,26 +231,62 @@ func removeElements(head *ListNode, val int) *ListNode {
 // 翻转链表II
 //给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
 //
-func reverseBetween(head *ListNode, left int, right int) *ListNode {
-	// 思路:先保存 left 前面的 head
-	// 然后保存 right 后面的
-	// 中间部分变成一个单独的链表，然后直接进行反转
+//思路:先保存 left 前面的 head
+//然后保存 right 后面的
+//中间部分变成一个单独的链表，然后直接进行反转
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	// 因为头节点有可能发生变化，使用虚拟头节点可以避免复杂的分类讨论
+	dummyNode := &ListNode{Val: -1}
+	dummyNode.Next = head
 
-	var head1 = head
-	for i := 1; i < left; i++ {
-		head = head.Next
+	pre := dummyNode
+	// 第 1 步：从虚拟头节点走 left - 1 步，来到 left 节点的前一个节点
+	// 建议写在 for 循环里，语义清晰
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
 	}
 
-	for i := 0; i < right-left; i++ {
-		head1 = head1.Next
+	// 第 2 步：从 pre 再走 right - left + 1 步，来到 right 节点
+	rightNode := pre
+	for i := 0; i < right-left+1; i++ {
+		rightNode = rightNode.Next
 	}
 
-	return head1.Next
+	// 第 3 步：切断出一个子链表（截取链表）
+	leftNode := pre.Next
+	curr := rightNode.Next
 
-	//for i := 0; i < right; i++ {
-	//	head1 = head1.Next
-	//}
-	//right1 := &ListNode{Next: head1}
-	//head1.Next = right1.Next
-	//return head1
+	// 注意：切断链接
+	pre.Next = nil
+	rightNode.Next = nil
+
+	// 第 4 步：同第 206 题，反转链表的子区间
+	reverseLinkedList(leftNode)
+
+	// 第 5 步：接回到原来的链表中
+	pre.Next = rightNode
+	leftNode.Next = curr
+	return dummyNode.Next
+}
+
+func reverseLinkedList(head *ListNode) {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+}
+
+// 递归翻转链表
+func reverse(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	last := reverse(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
 }
