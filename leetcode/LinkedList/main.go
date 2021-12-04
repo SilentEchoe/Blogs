ced6d64274286cf5c51bde356cc8610956208db1
kai_wang <871779972@qq.com>
2021-12-04 17:13:28 +0800

    add New func

--- leetcode/LinkedList/main.go
+++ leetcode/LinkedList/main.go
@@ -282,3 +282,25 @@ func reverse(head *ListNode) *ListNode {
 	head.Next = nil
 	return last
 }
+
+var successor = &ListNode{}
+
+func reverseN(head *ListNode, n int) *ListNode {
+	if n == 1 {
+		successor = head.Next
+		return head
+	}
+
+	var last = reverseN(head.Next, n-1)
+	head.Next.Next = head
+	head.Next = successor
+	return last
+}
+
+func NewReverseBetween(head *ListNode, n int, m int) *ListNode {
+	if m == 1 {
+		return reverseN(head, n)
+	}
+	head.Next = reverseBetween(head.Next, m-1, n-1)
+	return head
+}
