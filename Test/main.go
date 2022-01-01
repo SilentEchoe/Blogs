441bcd2596d190fe8ad52011fded1f5ff828866b
kai_wang <871779972@qq.com>
2022-01-01 16:37:06 +0800

    update test

--- Test/main.go
+++ Test/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var arrOne []int
-	fmt.Println(len(arrOne))
+	arr := [...]int{1, 2, 3}
+	arrOne := arr[0:3]
+	arr[1] = 4
+	fmt.Println(arrOne)
+}
+
+func doWork() {
+
 }
