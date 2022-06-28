1dfb352d9f05cb908151f4a062c05704161651fd
kai <871779972@qq.com>
2022-06-28 17:21:49 +0800

    cut func

--- test/main.go
+++ test/main.go
@@ -2,44 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/minio/minio-go/v6"
-	"log"
+	"strings"
 )
 
 func main() {
+	var rec = "12=13"
+	k, v, ok := strings.Cut(rec, "=")
+	if !ok {
+		fmt.Println("解析失败")
 
-	endpoint := "localhost:9090"
-	accessKeyID := "AKIAIOSFODNN7EXAMPLE"
-	secretAccessKey := "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-	useSSL := false
-
-	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
-	if err != nil {
-		log.Fatalln("创建 MinIO 客户端失败", err)
-		return
 	}
-	log.Printf("创建 MinIO 客户端成功")
-
-	if err != nil {
-		fmt.Println("错误信息", err)
-	}
-
-	listBuckets, _ := minioClient.ListBuckets()
-	fmt.Println(listBuckets)
-
-	// 创建一个叫 mybucket 的存储桶。
-	bucketName := "mybucket"
-	location := ""
-
-	err = minioClient.MakeBucket(bucketName, location)
-	if err != nil {
-		// 检查存储桶是否已经存在。
-		exists, err := minioClient.BucketExists(bucketName)
-		if err == nil && exists {
-			log.Printf("存储桶 %s 已经存在", bucketName)
-		} else {
-			log.Fatalln("查询存储桶状态异常", err, exists)
-		}
-	}
-	log.Printf("创建存储桶 %s 成功", bucketName)
+	fmt.Println(k)
+	fmt.Println(v)
 }
