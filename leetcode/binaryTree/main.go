151f3b179dc445eac5d4345fef9a84fc92893563
kai <871779972@qq.com>
2022-07-25 09:32:14 +0800

    maxDepth fix

--- leetcode/binaryTree/main.go
+++ leetcode/binaryTree/main.go
@@ -8,18 +8,20 @@ type TreeNode struct {
 	right *TreeNode
 }
 
-//func traverse(root *TreeNode) {
-//
-//	if root == nil {
-//		return
-//	}
-//
-//	// 前序位置
-//	traverse(root.left)
-//	// 中序位置
-//	traverse(root.right)
-//	// 后序位置
-//}
+// 所谓的 traverse 函数就是一个能遍历所有节点的一个函数，与遍历数组或者链表没有本质区别
+//前序位置的代码在刚刚进入一个二叉树节点的时候执行；
+//后序位置的代码在将要离开一个二叉树节点的时候执行；
+//中序位置的代码在一个二叉树节点左子树都遍历完，即将开始遍历右子树的时候执行。
+func traverseDemo(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	// 前序位置
+	traverse(root.left)
+	// 中序位置
+	traverse(root.right)
+	// 后序位置
+}
 
 func main() {
 
--- leetcode/binaryTree/maxDepth.go
+++ leetcode/binaryTree/maxDepth.go
@@ -7,9 +7,25 @@ var res = 0
 var depth = 0
 
 func maxDepth(root *TreeNode) int {
+	if root != nil {
+		return 0
+	}
+
+	// 记录左边最深的节点
+	leftMax := maxDepth(root.left)
+	rightMax := maxDepth(root.right)
+	var res int = 0
+	if leftMax > rightMax {
+		res = leftMax + 1
+	} else {
+		res = rightMax + 1
+	}
 	traverse(root)
 	return res
 }
+
+//前序位置是进入一个节点的时候，后序位置是离开一个节点的时候，depth 记录当前递归到的节点深度
+//把 traverse 理解成在二叉树上游走的一个指针，所以当然要这样维护
 
 func traverse(root *TreeNode) {
 	if root == nil {
