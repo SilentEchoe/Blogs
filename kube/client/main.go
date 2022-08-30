f754588f96f8d2ced9170e255ab390e370d088a8
kai <871779972@qq.com>
2022-08-30 22:36:11 +0800

    create controller

--- kube/client/main.go
+++ kube/client/main.go
@@ -1,62 +1,43 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"LearningNotes-Go/kube/client/pkg"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"log"
 )
 
 func main() {
-	client()
-}
-
-//func restClient() {
-//	// config 可以从指定的文件中获取，例如：clientcmd.RecommendedHomeFile 从 home 文件中
-//	// 如果不传递参数，它会从classname 中获取
-//	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
-//	if err != nil {
-//		fmt.Println(err)
-//		panic(err)
-//	}
-//
-//	config.GroupVersion = &v1.SchemeGroupVersion
-//	config.NegotiatedSerializer = scheme.Codecs
-//	config.APIPath = "/api"
-//
-//	// client
-//	restClient, err := rest.RESTClientFor(config)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//
-//	// get data
-//	pod := v1.Pod{}
-//	err = restClient.Get().Namespace("ingress-nginx").Resource("pods").Name("ingress-nginx-admission-create-w8sd2").Do(context.Background()).Into(&pod)
-//	if err != nil {
-//		panic(err)
-//	} else {
-//		fmt.Println(pod.Name)
-//	}
-//}
-
-func client() {
+	// 获取 Config
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
-		fmt.Println(err)
-		panic(err)
+		inClusterConfig, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalln("can't get config ")
+		}
+		config = inClusterConfig
 	}
 
+	// client
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err)
+		log.Fatalln("can't create client")
 	}
-	coreV1 := clientset.CoreV1()
-	pod, err := coreV1.Pods("default").Get(context.Background(), "test", v1.GetOptions{})
-	if err != nil {
-		println(err)
-	} else {
-		println(pod.Name)
-	}
+
+	// informer
+
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	serviceInformer := factory.Core().V1().Services()
+	ingressInformer := factory.Networking().V1().Ingresses()
+
+	// add event handler
+	controller := pkg.NewController(clientset, serviceInformer, ingressInformer)
+
+	// informer.Start
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	factory.WaitForCacheSync(stopCh)
+	controller.Run(stopCh)
 }
