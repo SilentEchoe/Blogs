7e2d933efd4be18c006d587fa91a6190d652d1d2
anonymity <33675262+AnAnonymousFriend@users.noreply.github.com>
2022-12-13 17:06:46 +0800

    logestPalindrome

--- leetcode/array/main.go
+++ leetcode/array/main.go
@@ -4,13 +4,16 @@
 双指针技巧：主要分为两类，左右指针和快慢指针
 左右指针就是两个相向或者相背而行。
 快慢指针就是两个指针同向而行，一快一慢。
+
+找回文串技巧：从中心向两端扩展的双指针技巧
+如果回文串的长度为奇数，则它有一个中心字符；如果长度为偶数，则有两个中心字符串
 */
 package main
 
+import "fmt"
+
 func main() {
-	numbers := []int{2, 7, 11, 15}
-	target := 9
-	twoSum(numbers, target)
+	palindrome("123321", 0, 6)
 }
 
 // Code.167 两数之和-输入有序数组
@@ -44,7 +47,6 @@ func removeDuplicates(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-
 	slow := 0
 	fast := 0
 	for fast < len(nums) {
@@ -55,4 +57,95 @@ func removeDuplicates(nums []int) int {
 		fast++
 	}
 	return slow + 1
+}
+
+// Code.27 移除元素
+// 同Code.26 依然使用快慢指针的方法
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
+
+// Code.704 二分查找
+// 标准使用数组二分查找
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	left, right := 0, len(nums)-1
+	for left <= right {
+		var mid = (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
+// 反转数组
+func reverse(sum []int) {
+	left := 0
+	right := len(sum) - 1
+
+	for left < right {
+		var tem = sum[left]
+		sum[left] = sum[right]
+		sum[left] = tem
+		left++
+		right--
+	}
+}
+
+// 回文字符串
+
+// palindrome 辅助函数
+// 在s中寻找以 s[l] 和 s[r] 为中心的最长回文串
+func palindrome(s string, left int, right int) string {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		// 双指针，向两边展开
+		left--
+		right++
+	}
+	fmt.Println(s[left:right])
+	return s[left:right]
+}
+
+// 最长回文串
+func logestPalindrome(s string) string {
+	res := ""
+
+	for i := 0; i < len(s); i++ {
+		s1 := palindrome(s, i, i)
+		s2 := palindrome(s, i, i+1)
+
+		if len(res) < len(s1) {
+			res = s1
+		} else {
+			res = res
+		}
+		if len(res) < len(s1) {
+			res = s2
+		} else {
+			res = res
+		}
+
+	}
+	return res
 }
