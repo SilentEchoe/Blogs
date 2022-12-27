ee43a742e5041522b8570df26d25b01b53e46794
anonymity <33675262+AnAnonymousFriend@users.noreply.github.com>
2022-12-27 15:37:47 +0800

    矩阵度螺旋遍历

--- leetcode/array/main.go
+++ leetcode/array/main.go
@@ -262,3 +262,57 @@ func ArryReverse(arr []int) {
 	}
 
 }
+
+// 矩阵度螺旋遍历
+
+func spiralOrder(matrix [][]int) []int {
+	m := len(matrix)
+	n := len(matrix[0])
+	upper_bound := 0
+	lower_bound := m - 1
+	left_bound := 0
+	right_bound := n - 1
+
+	var res []int
+
+	for len(res) < m*n {
+		if upper_bound <= lower_bound {
+			// 在顶部从左向右遍历
+			for j := left_bound; j <= right_bound; j++ {
+				res = append(res, matrix[upper_bound][j])
+			}
+			// 上边界下移
+			upper_bound++
+		}
+
+		if left_bound <= right_bound {
+			// 在右侧从上向下遍历
+			for i := upper_bound; i <= lower_bound; i++ {
+				res = append(res, matrix[i][right_bound])
+			}
+			// 右边界左移
+			right_bound--
+		}
+
+		if upper_bound <= lower_bound {
+			// 在底部从右向左遍历
+			for j := right_bound; j >= left_bound; j-- {
+				res = append(res, matrix[lower_bound][j])
+			}
+			// 下边界上移
+			lower_bound--
+		}
+
+		if left_bound <= right_bound {
+			// 在左侧从下向上遍历
+			for i := lower_bound; i >= upper_bound; i-- {
+				res = append(res, matrix[i][left_bound])
+			}
+			// 左边界右移
+			left_bound++
+		}
+
+	}
+	return res
+
+}
