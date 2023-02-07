4cfa2c2b4dec6b05221717bc7ab8744f4c14c357
anonymity <871779972@qq.com>
2023-02-07 16:04:20 +0800

    dynamicClient

--- kube/client/main.go
+++ kube/client/main.go
@@ -3,37 +3,59 @@ package main
 import (
 	"context"
 	"fmt"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	// config 可以从指定的文件中获取，例如：clientcmd.RecommendedHomeFile 从 home 文件中
-	// 如果不传递参数，它会从classname 中获取
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
 	}
 
-	config.GroupVersion = &v1.SchemeGroupVersion
-	config.NegotiatedSerializer = scheme.Codecs
-	config.APIPath = "/api"
-
-	// client
-	restClient, err := rest.RESTClientFor(config)
+	dynamicClient, err := dynamic.NewForConfig(config)
 	if err != nil {
-		fmt.Println(err)
+		panic(err.Error())
 	}
 
-	// get data
-	pod := v1.Pod{}
-	err = restClient.Get().Namespace("default").Resource("pods").Name("redis-79f984f6b5-vjxxw").Do(context.Background()).Into(&pod)
+	// dynamicClient 的唯一关联方法所需的入参
+	gvr := schema.GroupVersionResource{Version: "v1", Resource: "pods"}
+
+	// 使用dunamicClient 的查询列表方法.查询指定 namespace 下的所有Pod
+	// 注意此方法返回的数据结构类型是 UnstructureList
+	unstructObj, err := dynamicClient.
+		Resource(gvr).
+		Namespace("kube-system").
+		List(context.TODO(), metav1.ListOptions{Limit: 100})
+
 	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println(pod.Name)
+		panic(err.Error())
 	}
+
+	// 实例化一个PodList数据结构，用于接收从unstructObj转换后的结果
+	podList := &apiv1.PodList{}
+
+	// 转换
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstructObj.UnstructuredContent(), podList)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 表头
+	fmt.Printf("namespace\t status\t\t name\n")
+
+	// 每个pod都打印namespace、status.Phase、name三个字段
+	for _, d := range podList.Items {
+		fmt.Printf("%v\t %v\t %v\n",
+			d.Namespace,
+			d.Status.Phase,
+			d.Name)
+	}
+
 }
