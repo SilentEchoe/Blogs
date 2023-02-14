db70c8c212f0d243adb112b5cdf2ebd4934aa38f
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-02-14 17:47:52 +0800

    test

--- test/go.mod
+++ test/go.mod
@@ -3,6 +3,8 @@ module LearningNotes-Go/test
 go 1.18
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/google/uuid v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -15,6 +17,7 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/redis/go-redis/v9 v9.0.2 // indirect
 	github.com/rs/xid v1.2.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
--- test/main.go
+++ test/main.go
@@ -1,21 +1,62 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"time"
+	"github.com/redis/go-redis/v9"
 )
 
-const token = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwidWlkIjoiZjkzN2JiMzctYmFmNC0xMWVjLWJkYWYtY2E0ZWM0MTdkNTBlIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4iLCJmZWRlcmF0ZWRfY2xhaW1zIjp7ImNvbm5lY3Rvcl9pZCI6InN5c3RlbSIsInVzZXJfaWQiOiJhZG1pbiJ9LCJzaWQiOiIiLCJsb2dpbl90eXBlIjoiIiwiYXVkIjpudWxsLCJleHAiOjE2NzUzMDM1MDd9.f4_acDc3daMofLh1Rp0o1DsEzIUwN6R63lGRvsQhCqo`
-
 func main() {
-	url := "http://10.3.70.149:30001/"
-	//startDate := time.Now().ParseDuration("-24h") - time.Hour
-	d, _ := time.ParseDuration("-24h")
-	startDate := time.Now().Add(d).Unix()
-	endDate := time.Now().Unix()
+	ctx := context.Background()
 
-	//url := "/api/aslan/stat/dashboard/build?startDate=%s&endDate=%s"
-	//fmt.Sprintf(url,)
-	url = fmt.Sprintf(url+"/api/aslan/stat/dashboard/build?startDate=%d&endDate=%d", startDate, endDate)
-	fmt.Println("%s", url)
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	val, err := rdb.Get(ctx, "devops_dashboard_build").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	str1 := []byte(`{"total":"45","success":22}`)
+
+	var revMsg1 test
+	err = json.Unmarshal(str1, &revMsg1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("test")
+	fmt.Println(revMsg1)
+
+	str := []byte(`{"FromUid1":"100","FromName":"HANASHAN"}`)
+
+	var revMsg receiveMessage
+	err = json.Unmarshal(str, &revMsg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(revMsg)
+
+}
+
+//接收普通消息结构体
+type receiveMessage struct {
+	FromUid1 string //发送者uid
+	FromName string //发送者名字
+}
+
+type test struct {
+	Total   string
+	success int
+}
+
+type Data struct {
+	date    string
+	success int
+	failure int
+	total   int
 }
