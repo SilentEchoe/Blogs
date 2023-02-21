fc34211c033c120f5dce65799ef2218b5f373282
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-02-21 11:20:23 +0800

    test yaml

--- test/main.go
+++ test/main.go
@@ -1,36 +1,44 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
 
 type KafkaCluster struct {
-	Rule Rule `yaml:"rule"`
-}
-
-type Rule struct {
-	Home map[string]string `yaml:"home"`
+	Rule map[string]string `yaml:"rule"`
+	test map[string]string
 }
 
 func main() {
 	var c KafkaCluster
-	//读取yaml配置文件, 将yaml配置文件，转换struct类型
+	////读取yaml配置文件, 将yaml配置文件，转换struct类型
 	conf := c.getConf()
 
-	//将对象，转换成json格式
-	data, err := json.Marshal(conf)
+	rules := []string{"/api/v1/home/app", "/api/v1/home/log"}
+	test2 := getRuleKeyByPath(rules, conf)
 
-	if err != nil {
-		fmt.Println("err:\t", err.Error())
-		return
+	if _, ok := conf.Rule["get_app"]; ok {
+		fmt.Println("存在ok")
 	}
 
-	//最终以json格式，输出
-	fmt.Println("data:\t", string(data))
-	//fmt.Println(data)
+	fmt.Println("该用户可访问的接口为:", test2)
+
+}
+
+func getRuleKeyByPath(rule []string, conf *KafkaCluster) map[string]string {
+	result := make(map[string]string)
+
+	for _, val := range rule {
+		for rulekey, values := range conf.Rule {
+			if values == val {
+				result[rulekey] = values
+			}
+		}
+	}
+
+	return result
 }
 
 //读取Yaml配置文件,
