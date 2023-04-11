7c8a9fe504a08f8a01a1e80bbd6e42663821eac0
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-04-11 16:25:20 +0800

    create deployment to nginx

--- kube/client/main.go
+++ kube/client/main.go
@@ -2,60 +2,92 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"flag"
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
 )
 
 func main() {
-	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		fmt.Println(err)
+		panic(err.Error())
+	}
+
+	// 创建连接
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deploymentsClient := clientset.AppsV1().Deployments("yunhang")
+	createDeployment(deploymentsClient)
+}
+
+func createDeployment(deploymentsClient v1.DeploymentInterface) {
+	var r apiv1.ResourceRequirements
+	j := `{"limits": {"cpu":"200m", "memory": "1Gi"}, "requests": {"cpu":"200m", "memory": "1Gi"}}`
+	json.Unmarshal([]byte(j), &r)
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "nginx",
+			Labels: map[string]string{
+				"app": "nginx",
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr2(1),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "nginx",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "nginx",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{Name: "nginx",
+							Image:           "nginx:1.13.5-alpine",
+							Resources:       r,
+							ImagePullPolicy: "IfNotPresent",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Println("Creating deployment...")
+	result, err := deploymentsClient.Create(context.Background(), deployment, metav1.CreateOptions{})
+	if err != nil {
 		panic(err)
 	}
-
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// dynamicClient 的唯一关联方法所需的入参
-	gvr := schema.GroupVersionResource{Version: "v1", Resource: "pods"}
-
-	// 使用dunamicClient 的查询列表方法.查询指定 namespace 下的所有Pod
-	// 注意此方法返回的数据结构类型是 UnstructureList
-	unstructObj, err := dynamicClient.
-		Resource(gvr).
-		Namespace("kube-system").
-		List(context.TODO(), metav1.ListOptions{Limit: 100})
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// 实例化一个PodList数据结构，用于接收从unstructObj转换后的结果
-	podList := &apiv1.PodList{}
-
-	// 转换
-	err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstructObj.UnstructuredContent(), podList)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// 表头
-	fmt.Printf("namespace\t status\t\t name\n")
-
-	// 每个pod都打印namespace、status.Phase、name三个字段
-	for _, d := range podList.Items {
-		fmt.Printf("%v\t %v\t %v\n",
-			d.Namespace,
-			d.Status.Phase,
-			d.Name)
-	}
-
+	fmt.Printf("Created deployment %s.\n", result.GetObjectMeta().GetName())
 }
+
+func int32Ptr2(i int32) *int32 { return &i }
