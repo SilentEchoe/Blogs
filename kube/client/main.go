e46303aa7e3d74647bbbe2e420abf14376d85e17
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-04-19 22:11:05 +0800

    数组模版

--- kube/client/main.go
+++ kube/client/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"path/filepath"
+	"text/template"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 	"k8s.io/klog/v2"
-	"text/template"
 )
 
 type BackuoPod struct {
@@ -25,10 +32,20 @@ type BackuoPod struct {
 	OutSftpEndpoint  string
 	OutSftpFile      string
 	OutVolumeFile    string
+	MasterIps        []string
+}
+
+var kubeClientSet *kubernetes.Clientset
+
+func Init() {
+	kubeClientSet = newKubeClientSet()
+	if kubeClientSet == nil {
+		panic("init kube client failed")
+	}
 }
 
 func main() {
-	tmpl, err := template.ParseFiles("./backup-devops.yaml")
+	tmpl, err := template.ParseFiles("./backup.yaml")
 	if err != nil {
 		fmt.Println("create template failed, err:", err)
 		return
@@ -40,6 +57,7 @@ func main() {
 		ImageName:       "nginx:latest",
 		InMinioBucket:   "bucket",
 		InMongoEndpoint: "mongodb://root:zadig@kr-mongodb:27017",
+		MasterIps:       []string{"devops", "save"},
 	}
 
 	var bs bytes.Buffer
@@ -52,7 +70,35 @@ func main() {
 	_, _, err = decoder.Decode(bs.Bytes(), nil, obj)
 	if err != nil {
 		panic(err)
+
 	}
-	klog.Infof("%#v", obj)
+	//klog.Infof("%#v", obj)
+
+	klog.Infof(string(bs.Bytes()))
+
+}
+
+func newKubeClientSet() *kubernetes.Clientset {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// 使用kubeconfig中的当前上下文,加载配置文件
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 创建clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return clientset
 
 }
