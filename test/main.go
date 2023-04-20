42af9d6f0df2b39116e48459ce3a5837331ec77b
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-04-20 21:14:17 +0800

    feat: 构建基础

--- test/main.go
+++ test/main.go
@@ -1,1 +1,46 @@
 package main
+
+import "fmt"
+
+type PodTemplate interface {
+	CreatePod()
+	StoragePattern()
+}
+
+type Pod struct {
+	PodName   string
+	Namespace string
+	Images    string
+}
+
+type BacupKubernetesSavePod struct {
+	Pod
+	EtcdAddress string
+}
+
+type BacupDevopsSavePod struct {
+	Pod
+	MinioAddress string
+	MongoAddress string
+	MysqlAddress string
+}
+
+type RestoreDevopsPod struct {
+	Pod
+}
+
+// 创建pod
+func (b *BacupKubernetesSavePod) CreatePod() {
+	fmt.Println("创建pod")
+}
+
+func (b *BacupKubernetesSavePod) StoragePattern() {
+	//TODO implement me
+	fmt.Println("implement me")
+}
+
+func main() {
+	var podTemplate PodTemplate
+	podTemplate = &BacupKubernetesPod{}
+	podTemplate.CreatePod()
+}
