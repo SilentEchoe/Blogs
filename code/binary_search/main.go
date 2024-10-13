6f28c386e8d9b4d53775ff692edbb3cace95ba86
anonymity <kai.wang.info@gmail.com>
2024-10-14 01:17:32 +0800

    合并

--- code/binary_search/main.go
+++ code/binary_search/main.go
@@ -1,16 +1,20 @@
 /* 二分查找 */
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var nums1 = []int{1, 2, 3, 0, 0, 0}
+	var nums2 = []int{2, 5, 6}
+
+	merge(nums1, 3, nums2, 3)
 }
 
-//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
-//写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
-//输入: nums = [-1,0,3,5,9,12], target = 9
-//输出: 4
-//解释: 9 出现在 nums 中并且下标为 4
-
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+// 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+// 输出: 4
+// 解释: 9 出现在 nums 中并且下标为 4
 func search(nums []int, target int) int {
 	low := 0
 	high := len(nums) - 1
@@ -29,4 +33,59 @@ func search(nums []int, target int) int {
 	}
 
 	return -1
+}
+
+// Leetcode 88.合并两个有序数据
+// 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+// 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+// 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// 输出：[1,2,2,3,5,6]
+// 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+// 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+// 思路一：直接合并两个数组，然后使用快速排序重新排序
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	newNums1 := nums1[0:m]
+	newNums2 := nums2[0:n]
+
+	newNums := append(newNums1, newNums2...)
+
+	QuickSort(newNums, 0, len(newNums)-1)
+
+	fmt.Println(newNums)
+}
+
+func partition(list []int, low, high int) int {
+	pivot := list[low] //导致 low 位置值为空
+	for low < high {
+		//high指针值 >= pivot high指针👈移
+		for low < high && pivot <= list[high] {
+			high--
+		}
+		//填补low位置空值
+		//high指针值 < pivot high值 移到low位置
+		//high 位置值空
+		list[low] = list[high]
+		//low指针值 <= pivot low指针👉移
+		for low < high && pivot >= list[low] {
+			low++
+		}
+		//填补high位置空值
+		//low指针值 > pivot low值 移到high位置
+		//low位置值空
+		list[high] = list[low]
+	}
+	//pivot 填补 low位置的空值
+	list[low] = pivot
+	return low
+}
+
+func QuickSort(list []int, low, high int) {
+	if high > low {
+		//位置划分
+		pivot := partition(list, low, high)
+		//左边部分排序
+		QuickSort(list, low, pivot-1)
+		//右边排序
+		QuickSort(list, pivot+1, high)
+	}
 }
