cfae95c67ed9167c1d4e8c144c8bf3cd54fa46c4
anonymity <kai.wang.info@gmail.com>
2024-10-14 00:49:55 +0800

    二分查询法

--- code/binary_search/main.go
+++ code/binary_search/main.go
@@ -1,5 +1,32 @@
-package binary_search
+/* 二分查找 */
+package main
 
 func main() {
 
+}
+
+//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+//写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+//输入: nums = [-1,0,3,5,9,12], target = 9
+//输出: 4
+//解释: 9 出现在 nums 中并且下标为 4
+
+func search(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := nums[mid]
+		if guess == target {
+			return mid
+		}
+		if guess > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
 }
