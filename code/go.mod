15fc1035a015331d2eaeed1b44187312c4ba850a
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-01-18 23:48:18 +0800

    save

---
+++ code/go.mod
@@ -0,0 +1,5 @@
+module Blogs/code
+
+go 1.22.1
+
+require github.com/fatih/structs v1.1.0
--- code/main.go
+++ code/main.go
@@ -1,12 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	mate := []string{"1", "2", "3"}
-	mateInfo(mate)
-}
 
-func mateInfo(args ...any) {
-	fmt.Println(args)
 }
