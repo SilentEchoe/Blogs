4fe013ce097c273ae4293658d3d689c6817b3466
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-03-13 09:29:30 +0800

    code.11

---
+++ code/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
+}
+
+// LeetCode.11 盛最多水的容器
+// 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+// 返回容器可以储存的最大水量。
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	ans := 0
+	for left < right {
+		area := min(height[left], height[right]*(right-left))
+		fmt.Println(area)
+		ans = max(ans, area)
+		if height[left] <= height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return ans
+}
+
+// 方法2：自定义 min 函数
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
