9935a136ee1dc853dcfbbdb36a90811febaa30b6
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-06-12 17:07:49 +0800

    SICP

--- code/sicp.go
+++ code/sicp.go
@@ -1,7 +1,35 @@
 package main
 
+type Pair struct {
+	Car interface{}
+	Cdr interface{}
+}
+
+func Cons(a, b interface{}) *Pair {
+	return &Pair{Car: a, Cdr: b}
+}
+
+func Car(p Pair) interface{} {
+	return p.Car
+}
+
+func Cdr(p Pair) interface{} {
+	return p.Cdr
+}
+
 func main() {
 
 }
 
-type 
+func GCD(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return GCD(b, a%b)
+}
+
+func MakeRat(n, d int) *Pair {
+
+	g := GCD(n, d)
+	return Cons(n/g, d/g) // 返回一个 pair：最简分数
+}
