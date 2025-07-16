3a4f4b57c41b92ec8c065d761f28a907571a02bc
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-07-16 18:20:03 +0800

    SICP

--- code/main.go
+++ code/main.go
@@ -13,10 +13,10 @@ func factorial(n int) int {
 	return n * factorial(n-1)
 }
 
-func factorial(n int) int {
-	result := 1
-	for i := 2; i <= n; i++ {
-		result *= i
-	}
-	return result
-}
+//func factorial(n int) int {
+//	result := 1
+//	for i := 2; i <= n; i++ {
+//		result *= i
+//	}
+//	return result
+//}
