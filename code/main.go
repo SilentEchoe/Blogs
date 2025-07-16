56c4062eb495ddde9fd90e2f908c01f79d7eef78
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-07-16 17:10:43 +0800

    构建过程抽象

--- code/main.go
+++ code/main.go
@@ -1,51 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7})
-	fmt.Println("sqrt(9) =", Sqrt(9))
-	fmt.Println("sqrt(2) =", Sqrt(2))
-	fmt.Println("sqrt(0.01) =", Sqrt(0.01))
+	fmt.Println(factorial(5))
 }
 
-func square(x int) int {
-	return x * x
-}
-
-// LeetCode.11 盛最多水的容器
-// 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
-// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
-// 返回容器可以储存的最大水量。
-func maxArea(height []int) int {
-	left := 0
-	right := len(height) - 1
-	ans := 0
-	for left < right {
-		area := min(height[left], height[right]) * (right - left)
-		fmt.Println(area)
-		ans = max(ans, area)
-		if height[left] <= height[right] {
-			left++
-		} else {
-			right--
-		}
+func factorial(n int) int {
+	if n == 1 {
+		return 1
 	}
-	return ans
+	return n * factorial(n-1)
 }
 
-func min(a, b int) int {
-	if a < b {
-		return a
+func factorial(n int) int {
+	result := 1
+	for i := 2; i <= n; i++ {
+		result *= i
 	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return result
 }
