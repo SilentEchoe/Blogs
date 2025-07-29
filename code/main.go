3f4399f4bf9337e7cea32df07fa0b765cb16ca58
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-07-29 17:03:08 +0800

    fib

--- code/main.go
+++ code/main.go
@@ -6,6 +6,16 @@ func main() {
 	fmt.Println(factorial(5))
 }
 
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
 func factorial(n int) int {
 	if n == 1 {
 		return 1
