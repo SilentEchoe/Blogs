668be032d91f566aefac347636c1b87c9dfcf4c2
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-10-14 17:52:44 +0800

    构建数据抽象

---
+++ code/sicp_2.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Pair struct {
+	Car interface{}
+	Cdr interface{}
+}
+
+func Cons(a, b interface{}) *Pair {
+	return &Pair{Car: a, Cdr: b}
+}
+
+func Car(p *Pair) interface{} {
+	return p.Car
+}
+
+func Cdr(p *Pair) interface{} {
+	return p.Cdr
+}
+
+func PrintList(list *Pair) {
+	for list != nil {
+		fmt.Println(Car(list))
+		next := Cdr(list)
+		if next == nil {
+			break
+		}
+		list = next.(*Pair)
+	}
+}
+
+func main() {
+	l := Cons(1, Cons(2, Cons(3, nil)))
+	PrintList(l)
+
+}
